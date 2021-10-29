384a2116fd4819c9a73ea18d24f13591edec49fb
m.yamazaki <m.yamazaki@fujitsu.com>
2021-10-29 23:52:11 +0900

    update qbench.cpp

--- ict/qbench.cpp
+++ ict/qbench.cpp
@@ -1,51 +1,104 @@
 #include <iostream>
+#include <iomanip>
+#include <time.h>
 #include <cppsim/state.hpp>
 #include <cppsim/circuit.hpp>
 #include <cppsim/observable.hpp>
 #include <cppsim/gate_factory.hpp>
 #include <cppsim/gate_merge.hpp>
+#include <cppsim/circuit_optimizer.hpp>
 
-int main(int argc, char *argv[]){
-    int nqubits = 20;
-    int depth = 9;
-    if (argc > 1) nqubits = atoi(argv[1]);
-    std::cout << "# argc=" << argc << std::endl;
-    std::cout << "# nqubits=" << nqubits << std::endl;
+double get_realtime(void)
+{
+    struct timespec t;
+    //clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t);
+    clock_gettime(CLOCK_REALTIME, &t);
+    return t.tv_sec + (double)t.tv_nsec*1e-9;
+}
 
-    QuantumState state(nqubits);
-    state.set_Haar_random_state();
+void bench(int nqubits, int depth, int lv_opt, int num_dmy, int num_meas){
+    double dt;
+    double tsum = 0;
+    double tsum2 = 0;
 
-    //build_curcuit
-    QuantumCircuit circuit(nqubits);
-    for (int i=0; i<nqubits; ++i){
-        circuit.add_RX_gate(i, std::rand());
-        circuit.add_RZ_gate(i, std::rand());
-    }
-    for (int i=0; i<nqubits; ++i)
-        circuit.add_CNOT_gate(i, (i+1)%nqubits);
-    for (int j=0; j<depth; ++j){
+    for (int i = 0; i < (num_dmy + num_meas); ++i) {
+        dt = -1*get_realtime();
+
+        QuantumState state(nqubits);
+        state.set_Haar_random_state();
+    
+        // Build Circuit
+        QuantumCircuit circuit(nqubits);
+        for (int i=0; i<nqubits; ++i){
+            circuit.add_RX_gate(i, std::rand());
+            circuit.add_RZ_gate(i, std::rand());
+        }
+        for (int i=0; i<nqubits; ++i)
+            circuit.add_CNOT_gate(i, (i+1)%nqubits);
+        for (int j=0; j<depth; ++j){
+            for (int i=0; i<nqubits; ++i){
+                circuit.add_RZ_gate(i, std::rand());
+                circuit.add_RX_gate(i, std::rand());
+                circuit.add_RZ_gate(i, std::rand());
+                circuit.add_CNOT_gate(i, (i+1)%nqubits);
+            }
+        }
         for (int i=0; i<nqubits; ++i){
             circuit.add_RZ_gate(i, std::rand());
             circuit.add_RX_gate(i, std::rand());
-            circuit.add_RZ_gate(i, std::rand());
-            circuit.add_CNOT_gate(i, (i+1)%nqubits);
+        }
+    
+        // Output information of quantum circuit
+        // std::cout << circuit << std::endl;
+    
+        // Circuit Optimize
+        if (lv_opt >= 0){
+            QuantumCircuitOptimizer opt;
+            if (lv_opt == 0) opt.optimize_light(&circuit);
+            else {
+                opt.optimize(&circuit, lv_opt);
+            }
+        }
+    
+        // Update State
+        circuit.update_quantum_state(&state);
+    
+        dt += get_realtime();
+        if (i >= num_dmy){
+            tsum += dt;
+            tsum2 += dt * dt;
         }
     }
-    for (int i=0; i<nqubits; ++i){
-        circuit.add_RZ_gate(i, std::rand());
-        circuit.add_RX_gate(i, std::rand());
-    }
-    circuit.update_quantum_state(&state);
-    /*
-    circuit.add_X_gate(0);
-    auto merged_gate = gate::merge(gate::CNOT(0,1),gate::Y(1));
-    circuit.add_gate(merged_gate);
+    double tavg = tsum / num_meas;
+    double tstd = sqrt(tsum2 / num_meas - tavg * tavg);
 
-    observable.add_operator(2.0, "X 2 Y 1 Z 0");
-    observable.add_operator(-3.0, "Z 2");
-    Observable observable(nqubits);
-    auto value = observable.get_expectation_value(&state);
-    std::cout << value << std::endl;
-    */
-    return 0;
+    std::cout << "opt= " << lv_opt << ", q= " << nqubits << ", time[s] " << tavg << " +- " << tstd << std::endl;
+    return;
 }
+
+int main(int argc, char *argv[]){
+    int num_dmy = 3;
+    int num_meas = 5;
+    int st_nq = 10;
+    int ed_nq = 20;
+    int depth = 9;
+    if (argc > 1) st_nq = atoi(argv[1]);
+    if (argc > 2) ed_nq = atoi(argv[2]);
+    std::cout << "# nqubits: " << st_nq << " ~ " << ed_nq << std::endl;
+
+    std::cout << std::scientific << std::setprecision(9);
+    int i = std::max(st_nq, ed_nq);
+    std::cout << "# dummy" << std::endl;
+    bench(i, 9, -1, 0, 3);
+
+    std::cout << "# start meas" << std::endl;
+    int stp=1;
+    if (st_nq > ed_nq) stp=-1;
+    ed_nq+=stp;
+    for (int lv_opt=-1; lv_opt<5; ++lv_opt){
+        for (int i=st_nq; i!=ed_nq; i+=stp){
+            bench(i, 9, lv_opt, num_dmy, num_meas);
+        }
+    }
+}
+
