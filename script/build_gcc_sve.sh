031ebaa9954d471a3ebcf3a2142bc77f5267e0a9
Takumi-Honda <honda.takumi@fujitsu.com>
2022-02-01 13:35:15 +0900

    Added a build script for GCC w/ SVE, OpenMP, and MPI

---
+++ script/build_gcc_sve.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# if duplicate-definition error was occured,
+# rm -rf include build
+
+GCC_COMMAND="$C_COMPILER"
+GXX_COMMAND="$CXX_COMPILER"
+
+if [ -z "$GCC_COMMAND" ]; then
+  GCC_COMMAND="gcc"
+fi
+
+if [ -z "$GXX_COMMAND" ]; then
+  GXX_COMMAND="g++"
+fi
+
+# if gcc/g++ version is less than 8, use gcc-8/g++-8
+GCC_VERSION=$($GCC_COMMAND -dumpfullversion -dumpversion | awk -F. '{printf "%2d%02d%02d", $1,$2,$3}')
+if [ "$GCC_VERSION" -lt 80000 ]; then
+  GCC_COMMAND=gcc-7
+elif [ "$GCC_VERSION" -lt 90000 ]; then
+  GCC_COMMAND=gcc
+fi
+
+GXX_VERSION=$($GXX_COMMAND -dumpfullversion -dumpversion | awk -F. '{printf "%2d%02d%02d", $1,$2,$3}')
+if [ "$GXX_VERSION" -lt 80000 ]; then
+  GXX_COMMAND=g++-7
+elif [ "$GXX_VERSION" -lt 90000 ]; then
+  GXX_COMMAND=g++
+fi
+
+mkdir ./build
+cd ./build
+cmake -G "Unix Makefiles" -D CMAKE_C_COMPILER=$GCC_COMMAND -D CMAKE_CXX_COMPILER=$GXX_COMMAND \
+  -D CMAKE_C_FLAGS="-march=armv8.1-a+sve -msve-vector-bits=512" -D CMAKE_CXX_FLAGS="-march=armv8.1-a+sve -msve-vector-bits=512" \
+  -D USE_SVE=Yes -D USE_OMP=Yes \
+  -D CMAKE_BUILD_TYPE=Release ..
+make -j 40
+make python
+cd ../
+
