132d3bc0683fd2f5394bf248bc4edb94146f31d7
Akihiko-Kasagi <kasagi@fx700-04-02.local>
2022-02-19 18:13:23 +0900

    update inner-outer pair

--- src/csim/update_ops_named_BSWAP.c
+++ src/csim/update_ops_named_BSWAP.c
@@ -24,6 +24,8 @@
 
 void BSWAP_gate(UINT target_qubit_index_0, UINT target_qubit_index_1,
     UINT num_qubit, CTYPE* state, ITYPE dim) {
+   // printf("#call BSWAP_gate(%d, %d, %d)\n", target_qubit_index_0, target_qubit_index_1, num_qubit);
+    //fflush(stdout);
     for (UINT i = 0; i < num_qubit; ++i) {
         SWAP_gate(
             target_qubit_index_0 + i, target_qubit_index_1 + i, state, dim);
@@ -34,6 +36,8 @@ void BSWAP_gate(UINT target_qubit_index_0, UINT target_qubit_index_1,
 
 void BSWAP_gate_mpi(UINT target_qubit_index_0, UINT target_qubit_index_1,
     UINT num_qubit, CTYPE* state, ITYPE dim, UINT inner_qc) {
+    //printf("#call BSWAP_gate_mpi(%d, %d, %d)\n", target_qubit_index_0, target_qubit_index_1, num_qubit);
+    //fflush(stdout);
 #if 0
     for (UINT i = 0; i < num_qubit; i++){
         SWAP_gate_mpi(target_qubit_index_0+i, target_qubit_index_1+i, state, dim, inner_qc);
@@ -54,6 +58,14 @@ void BSWAP_gate_mpi(UINT target_qubit_index_0, UINT target_qubit_index_1,
         for (UINT i = 0; i < num_qubit; i++) {
             SWAP_gate(
                 target_qubit_index_0 + i, target_qubit_index_1 + i, state, dim);
+        }
+        return;
+    }
+
+    if (right_qubit + num_qubit - 1 >= inner_qc) {  // all swaps are in inner
+        for (UINT i = 0; i < num_qubit; i++) {
+            SWAP_gate_mpi(target_qubit_index_0 + i, target_qubit_index_1 + i,
+                state, dim, inner_qc);
         }
         return;
     }
@@ -94,7 +106,8 @@ void BSWAP_gate_mpi(UINT target_qubit_index_0, UINT target_qubit_index_1,
     CTYPE* t = m->get_workarea(&dim_work, &num_work);
     const ITYPE rtgt_blk_dim = 1 << right_qubit;
     const UINT total_peer_procs = 1 << act_bs;
-    const UINT tgt_outer_rank = left_qubit - inner_qc;
+    const UINT tgt_outer_rank_gap = left_qubit - inner_qc;
+    const UINT tgt_inner_rank_gap = inner_qc - right_qubit;
 
     dim_work = get_min_ll(dim_work, dim >> (act_bs - 1));
 
@@ -103,18 +116,24 @@ void BSWAP_gate_mpi(UINT target_qubit_index_0, UINT target_qubit_index_1,
         CTYPE* t_send = t;
         CTYPE* t_recv = t + dim_work;
         for (UINT step = 1; step < total_peer_procs; ++step) {
-            const UINT peer_rank = rank ^ (step << tgt_outer_rank);
-            UINT rtgt_offset = (rank ^ step) * rtgt_blk_dim;
+            const UINT peer_rank = rank ^ (step << tgt_outer_rank_gap);
+            UINT rtgt_offset_index = ((rank>>tgt_outer_rank_gap) ^step);
+	    UINT offset_mask = (1 << tgt_inner_rank_gap) - 1;
+	    rtgt_offset_index &= offset_mask;
+            //UINT rtgt_offset_index = ((rank>>act_bs) ^step);
             const ITYPE num_rtgt_block = (dim / dim_work) >> act_bs;
-            const ITYPE num_elem_block = dim_work >> right_qubit;
-            CTYPE* si0 = state + rtgt_offset;
-            for (ITYPE i = 0; i < num_rtgt_block; ++i) {
+            const ITYPE num_elem_block = dim_work / rtgt_blk_dim;
+            //printf("step=%d, num_rtgt_block=%lld, num_elem_block=%lld\n", step, num_rtgt_block, num_elem_block);
+            //printf("rtgt_offset_index=%d, myrank=%d, peer_rank=%d\n", rtgt_offset_index, rank, peer_rank);
+            CTYPE* si0 = state;
+            for (UINT i = 0; i < num_rtgt_block; ++i) {
                 // gather
                 CTYPE* si = si0;
                 CTYPE* ti = t_send;
-                for (ITYPE k = 0; k < num_elem_block; ++k) {
+                for (UINT k = 0; k < num_elem_block; ++k) {
+		    UINT iter = i * num_elem_block + k;
+                    si = si0 + (rtgt_offset_index^(iter<<act_bs)) * rtgt_blk_dim ;
                     memcpy(ti, si, rtgt_blk_dim * sizeof(CTYPE));
-                    si += (rtgt_blk_dim << act_bs);
                     ti += rtgt_blk_dim;
                 }
 
@@ -124,12 +143,13 @@ void BSWAP_gate_mpi(UINT target_qubit_index_0, UINT target_qubit_index_1,
                 // scatter
                 si = t_recv;
                 ti = si0;
-                for (ITYPE k = 0; k < num_elem_block; ++k) {
+                for (UINT k = 0; k < num_elem_block; ++k) {
+		    UINT iter = i * num_elem_block + k;
+                    ti = si0 + (rtgt_offset_index^(iter<<act_bs)) * rtgt_blk_dim ;
                     memcpy(ti, si, rtgt_blk_dim * sizeof(CTYPE));
                     si += rtgt_blk_dim;
-                    ti += (rtgt_blk_dim << act_bs);
                 }
-                si0 += (dim_work << act_bs);
+                //si0 += (dim_work << act_bs);
             }
         }
     } else {  // rtgt_blk_dim >= dim_work
