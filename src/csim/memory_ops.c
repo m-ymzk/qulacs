328be89d7648b12bd85179ce26ff5eae24b5597a
Takumi-Honda <honda.takumi@fujitsu.com>
2022-02-20 15:04:57 +0900

    Revert "Added a memcpy function w/ zero fill"
    This reverts commit 7544d044bdf0fc3c35f73a4b174cb084d781c67c.

--- src/csim/memory_ops.c
+++ src/csim/memory_ops.c
@@ -43,7 +43,6 @@ void memcpy_sve(double* Out, double* In, ITYPE Num){
   ITYPE i;
   ITYPE vec_len = svcntd();
 
-#if 0
   if((Num >= vec_len) && ((Num % vec_len) == 0)){
     SV_FTYPE val;
 
@@ -59,74 +58,6 @@ void memcpy_sve(double* Out, double* In, ITYPE Num){
       Out[i] = In[i];
     }
   }
-#else
-
-#define ZFILL_DISTANCE 2
-#define PRF_DISTANCE ZFILL_DISTANCE
-#define CACHE_LINE_SIZE 256
-
-  if((Num >= (CACHE_LINE_SIZE/sizeof(double))) && ((Num % vec_len) == 0)){
-
-		// 
-    ITYPE aligned_start_index = 0;
-    for(i = 0; i < Num; i++){
-      if (((size_t)(&Out[i]) & (CACHE_LINE_SIZE-1)) == 0){
-        aligned_start_index = i;
-        break;
-      }
-      Out[i] = In[i];
-    }
-
-    const UINT thread_count = omp_get_max_threads();
-    const ITYPE remain_num = Num - aligned_start_index;
-		const ITYPE num_cachelines = remain_num / (CACHE_LINE_SIZE/sizeof(double));
-		const ITYPE block_size = (num_cachelines + thread_count - 1) / thread_count;
-    const ITYPE residual = remain_num % (CACHE_LINE_SIZE/sizeof(double));
-
-    if (num_cachelines){
-
-#pragma omp parallel private(i)
-      {
-        UINT thread_id = omp_get_thread_num();
-				ITYPE start_idx = thread_id * block_size * (CACHE_LINE_SIZE/sizeof(double)) + aligned_start_index;
-				ITYPE end_idx = (thread_id + 1) * block_size * (CACHE_LINE_SIZE/sizeof(double)) + aligned_start_index;
-				end_idx = (end_idx > (num_cachelines * (CACHE_LINE_SIZE/sizeof(double)) + aligned_start_index)) 
-										? num_cachelines * (CACHE_LINE_SIZE/sizeof(double)) + aligned_start_index : end_idx;
-
-        // Finish one cache line size earlier in consideration of alignment
-        // zfill distance + 1 cache line size
-        for(i = start_idx; i < end_idx; i += (vec_len << 2)){
-  				SV_FTYPE val0, val1, val2, val3;
-  				//__asm__ __volatile__("dc ZVA, %0\n\t" : : "r" (&Out[i + (vec_len << 2) * ZFILL_DISTANCE]) :"memory");
-  				__asm__ __volatile__("dc ZVA, %0\n\t" : : "r" (&Out[i]) :"memory");
-  				__builtin_prefetch(&Out[i], 1, 3);
-  				val0 = svld1(svptrue_b64(), &In[i]);
-  				val1 = svld1(svptrue_b64(), &In[i + vec_len]);
-  				val2 = svld1(svptrue_b64(), &In[i + vec_len * 2]);
-  				val3 = svld1(svptrue_b64(), &In[i + vec_len * 3]);
-  				svst1(svptrue_b64(), &Out[i], val0);
-  				svst1(svptrue_b64(), &Out[i + vec_len], val1);
-  				svst1(svptrue_b64(), &Out[i + vec_len * 2], val2);
-  				svst1(svptrue_b64(), &Out[i + vec_len * 3], val3);
-  				//__builtin_prefetch(&Out[i + (vec_len << 2) * PRF_DISTANCE], 1, 3);
-        } 
-      }
-    }
-
-		if (residual) {
-      for(i = residual; i > 0; i--){
-        Out[Num - i] = In[Num - i];
-      }
-		}
-  }else{
-#pragma omp parallel for private(i)
-    for(i = 0; i < Num; i++){
-      Out[i] = In[i];
-    }
-  }
-
-
-#endif
 
 }
 #endif
