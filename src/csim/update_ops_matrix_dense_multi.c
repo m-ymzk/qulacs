c4bc00b464585aeb3390603ec619f48ecbb24203
m.yamazaki <m.yamazaki@fujitsu.com>
2022-02-22 15:52:10 +0900

    Delete unneeded debug messages.

--- src/csim/update_ops_matrix_dense_multi.c
+++ src/csim/update_ops_matrix_dense_multi.c
@@ -31,7 +31,7 @@ void create_shift_mask_list_from_list_buf(
 void multi_qubit_dense_matrix_gate(const UINT* target_qubit_index_list,
     UINT target_qubit_index_count, const CTYPE* matrix, CTYPE* state,
     ITYPE dim) {
-    printf("#enter multi_qubit_dense_matrix_gate, %d\n", target_qubit_index_count);
+    //printf("#enter multi_qubit_dense_matrix_gate, %d\n", target_qubit_index_count);
     if (target_qubit_index_count == 1) {
         single_qubit_dense_matrix_gate(
             target_qubit_index_list[0], matrix, state, dim);
@@ -179,14 +179,13 @@ void multi_qubit_dense_matrix_gate_parallel(const UINT* target_qubit_index_list,
 void multi_qubit_dense_matrix_gate_mpi(const UINT* target_qubit_index_list,
     UINT target_qubit_index_count, const CTYPE* matrix, CTYPE* state,
     ITYPE dim, UINT inner_qc) {
-    printf("#enter multi_qubit_dense_matrix_gate_mpi, %d, %d, %d, %lld, %d\n",
-            target_qubit_index_list[0], target_qubit_index_list[1], target_qubit_index_count, dim, inner_qc);
+    //printf("#enter multi_qubit_dense_matrix_gate_mpi, %d, %d, %d, %lld, %d\n",
+    //        target_qubit_index_list[0], target_qubit_index_list[1], target_qubit_index_count, dim, inner_qc);
 
     UINT all_inner = 1;
     for (UINT i=0; i < target_qubit_index_count; ++i)
         if (target_qubit_index_list[i] >= inner_qc) all_inner = 0;
 
-    printf("#enter2 multi_qubit_dense_matrix_gate_mpi, %d\n", all_inner);
     if (all_inner)
         multi_qubit_dense_matrix_gate(target_qubit_index_list, target_qubit_index_count, matrix, state, dim);
     else
