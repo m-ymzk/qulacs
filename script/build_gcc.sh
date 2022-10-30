91e6c83dea6b04c3ceef836015a2ac8522bbbcd5
m.yamazaki <m.yamazaki@fujitsu.com>
2022-10-30 19:21:46 +0900

    add USE_MPI option to setup_gcc.sh

--- script/build_gcc.sh
+++ script/build_gcc.sh
@@ -4,13 +4,22 @@ set -eux
 
 GCC_COMMAND=${C_COMPILER:-"gcc"}
 GXX_COMMAND=${CXX_COMPILER:-"g++"}
+USE_GPU="${USE_GPU:-No}"
+USE_MPI="${USE_MPI:-No}"
+USE_TEST="${USE_TEST:-No}"
 
 mkdir -p ./build
 cd ./build
 if [ "${QULACS_OPT_FLAGS:-"__UNSET__"}" = "__UNSET__" ]; then
-  cmake -G "Unix Makefiles" -D CMAKE_C_COMPILER=$GCC_COMMAND -D CMAKE_CXX_COMPILER=$GXX_COMMAND -D CMAKE_BUILD_TYPE=Release -D USE_GPU:STR=No -D USE_TEST="${USE_TEST:-No}" ..
+    DEFINE_OPT_FLAGS=""
 else
-  cmake -G "Unix Makefiles" -D CMAKE_C_COMPILER=$GCC_COMMAND -D CMAKE_CXX_COMPILER=$GXX_COMMAND -D OPT_FLAGS="${QULACS_OPT_FLAGS}" -D CMAKE_BUILD_TYPE=Release -D USE_GPU:STR=No -D USE_TEST="${USE_TEST:-No}" ..
+    DEFINE_OPT_FLAGS="-D OPT_FLAGS=\"${QULACS_OPT_FLAGS}\""
 fi
+
+cmake -G "Unix Makefiles" -D CMAKE_C_COMPILER=$GCC_COMMAND -D CMAKE_CXX_COMPILER=$GXX_COMMAND \
+      $DEFINE_OPT_FLAGS \
+      -D CMAKE_BUILD_TYPE=Release -D USE_GPU=$USE_GPU -D USE_TEST=$USE_TEST -D USE_MPI=$USE_MPI \
+      ..
+
 make -j $(nproc)
 cd ../
